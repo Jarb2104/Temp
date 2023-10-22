65c88f17ac0162085c208ee78ae068fb57dd2bc9
Alejandro Reyes <Jarb2104@hotmail.com>
2023-10-22 09:35:25 -0600

    Enum Issue Example
    Issue with the NetEscapades.EnumGenerators nuget and Stride

---
+++ MyGame/EnumExmaple.cs
@@ -0,0 +1,24 @@
+﻿using Stride.Engine;
+using VoxelWorldBuilding.Types;
+
+namespace MyGame
+{
+    public class EnumExmaple : SyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override void Start()
+        {
+            // Initialization of the script.
+            foreach(MyEnum myEnum in MyEnumExtensions.GetValues())
+            {
+                Log.Verbose(myEnum.ToString());
+            }
+        }
+
+        public override void Update()
+        {
+            // Do stuff every new frame
+        }
+    }
+}
---
+++ MyGame/MyEnum.cs
@@ -0,0 +1,10 @@
+﻿using NetEscapades.EnumGenerators;
+
+namespace VoxelWorldBuilding.Types
+{
+    [EnumExtensions]
+    public enum MyEnum : byte
+    {
+        Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9, Item10, Item11, Item12, Item13
+    }
+}
